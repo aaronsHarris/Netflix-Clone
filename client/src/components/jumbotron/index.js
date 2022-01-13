803222481d9d3566b999ceac46dab99b353b3169
Aaron Harris <aaronsharris@gmail.com>
2022-01-13 13:09:22 -0600

    initial commit, picking up project after break

--- client/src/components/jumbotron/index.js
+++ client/src/components/jumbotron/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Container, Inner, Title, SubTitle, Image, Pane, Item } from "./styles/jumbotron";
 
-export default function Jumbotron({}) {
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
     return (
-        <Inner>
-        Hello again1
-        </Inner>
-    )
-}
+      <Item {...restProps}>
+        <Inner direction={direction}>{children}</Inner>
+      </Item>
+    );
+  }
+  
+  Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+  };
+  
+  Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+  };
+  
+  Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+  };
+  
+  Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+  };
+  
+  Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps} />;
+  };
